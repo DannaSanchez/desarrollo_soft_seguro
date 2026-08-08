ac07a62ebeb758edd8b3576bdd9aa85da4462169
DannaSanchez <sanchezdanna15@gmail.com>
2026-08-08 09:50:13 -0500

    protección de contraseña

--- models/DataBase.php
+++ models/DataBase.php
@@ -5,8 +5,8 @@
             $hostname = "localhost";
             $port = "3307";
             $database = "db_inventory";
-            $username = "root";
-            $password = "";
+            $username = getenv('DB_USER') ?: "app_user";
+            $password = getenv('DB_PASSWORD');
 			$pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$database;charset=utf8",$username,$password);
 			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			return $pdo;
